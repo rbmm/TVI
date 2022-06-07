5f4470885a58815f36d3dd24d88ec3cac854158a
rbmm <8209FBFB017DB41D@yopmail.com>
2022-06-07 22:10:48 +0300

    *

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by tvi.rc
+//
+#define IDR_MENU1                       101
+#define IDR_MENU2                       102
+#define ID_OPEN                         40001
+#define ID_EXIT                         40002
+#define ID_0_COPY                       40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ tvi.cpp
@@ -0,0 +1,502 @@
+#include "stdafx.h"
+
+_NT_BEGIN
+#include "resource.h"
+#include "../winZ/app.h"
+#include "../winz/mdi.h"
+#include "../winz/view.h"
+#include "../inc/initterm.h"
+
+HRESULT GetLastHresult(ULONG dwError = GetLastError())
+{
+	NTSTATUS status = RtlGetLastNtStatus();
+	return RtlNtStatusToDosErrorNoTeb(status) == dwError ? HRESULT_FROM_NT(status) : HRESULT_FROM_WIN32(dwError);
+}
+
+int CustomMessageBox(HWND hWnd, PCWSTR lpText, PCWSTR lpszCaption, UINT uType)
+{
+	PCWSTR pszName = 0;
+
+	switch (uType & MB_ICONMASK)
+	{
+	case MB_ICONINFORMATION:
+		pszName = IDI_INFORMATION;
+		break;
+	case MB_ICONQUESTION:
+		pszName = IDI_QUESTION;
+		break;
+	case MB_ICONWARNING:
+		pszName = IDI_WARNING;
+		break;
+	case MB_ICONERROR:
+		pszName = IDI_ERROR;
+		break;
+	}
+
+	MSGBOXPARAMS mbp = {
+		sizeof(mbp),
+		hWnd,
+		(HINSTANCE)&__ImageBase,
+		lpText, 
+		lpszCaption, 
+		(uType & ~MB_ICONMASK)|MB_USERICON,
+		MAKEINTRESOURCE(IDR_MENU1)
+	};
+
+	return MessageBoxIndirect(&mbp);
+}
+
+int ShowErrorBox(HWND hWnd, PCWSTR lpCaption, HRESULT dwError, UINT uType)
+{
+	int r = 0;
+	LPCVOID lpSource = 0;
+	ULONG dwFlags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS;
+
+	if (dwError & FACILITY_NT_BIT)
+	{
+		dwError &= ~FACILITY_NT_BIT;
+		dwFlags = FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS;
+
+		static HMODULE ghnt;
+		if (!ghnt && !(ghnt = GetModuleHandle(L"ntdll"))) return 0;
+		lpSource = ghnt;
+	}
+
+	PWSTR lpText;
+	if (FormatMessageW(dwFlags, lpSource, dwError, 0, (PWSTR)&lpText, 0, 0))
+	{
+		r = CustomMessageBox(hWnd, lpText, lpCaption, uType);
+		LocalFree(lpText);
+	}
+
+	return r;
+}
+
+struct AUX 
+{
+	ULONG MaxLevel = 0;
+	ULONG len = 0;
+	ULONG cb;
+	AUX(ULONG cb) : cb(cb) {}
+};
+
+PUCHAR IsValidTvi(ULONG level, PUCHAR buf, ULONG cb, AUX* p)
+{
+	if (cb < 1)
+	{
+		return 0;
+	}
+	
+	ULONG len = *buf++;
+	
+	if (len >= --cb)
+	{
+		return 0;
+	}
+
+	if (level > p->MaxLevel)
+	{
+		p->MaxLevel = level;
+	}
+
+	p->len += level + len + 2;
+
+	buf += len, cb -= len, level++;
+
+	while (cb && *buf)
+	{
+		if (!(buf = IsValidTvi(level, buf, cb, p)) || !(cb = p->cb))
+		{
+			return 0;
+		}
+	}
+
+	if (cb)
+	{
+		p->cb = cb - 1;
+		return buf + 1;
+	}
+
+	return 0;
+}
+
+struct ED 
+{
+	PWSTR buf;
+	ULONG cch;
+};
+
+const UCHAR* Dump(PCSTR prefix, const UCHAR* psz, ED* p)
+{
+	ULONG len = *psz++;
+	int cch = swprintf_s(p->buf, p->cch, L"%S%.*S\r\n", prefix, len, psz);
+	if (0 >= cch)
+	{
+		return 0;
+	}
+	psz += len;
+	prefix--;
+	p->buf += cch, p->cch -= cch;
+	if (*psz)
+	{
+		do 
+		{
+			if (!(psz = Dump(prefix, psz, p)))
+			{
+				return 0;
+			}
+		} while (*psz);
+	}
+
+	return psz + 1;
+}
+
+class ZTviTree : public ZMDIChildFrame, public ZView
+{
+	enum { ID_TV = 1 };
+
+	virtual ZWnd* getWnd()
+	{
+		return this;
+	}
+
+	void Copy(HWND hWnd);
+
+	static PUCHAR AddItems(HWND hwndTV, TVINSERTSTRUCT* tvis, PUCHAR psz)
+	{
+		tvis->item.lParam = (LPARAM)psz;
+	
+		if (HTREEITEM hItem = TreeView_InsertItem(hwndTV, tvis))
+		{
+			ULONG len = *psz++;
+			psz += len;
+
+			while (*psz)
+			{
+				tvis->hParent = hItem;
+				if (!(psz = AddItems(hwndTV, tvis, psz)))
+				{
+					return 0;
+				}
+			}
+
+			return psz + 1;
+		}
+
+		return 0;
+	}
+
+	virtual HWND CreateView(HWND hWndParent, int nWidth, int nHeight, PVOID lpCreateParams)
+	{
+		if (HWND hwnd = CreateWindowExW(0, WC_TREEVIEW, 0, WS_CHILD|WS_VISIBLE|
+			TVS_LINESATROOT|TVS_HASLINES|TVS_HASBUTTONS|TVS_DISABLEDRAGDROP|
+			TVS_TRACKSELECT|TVS_EDITLABELS, 0, 0, nWidth, nHeight, hWndParent, (HMENU)ID_TV, 0, 0))
+		{
+			if (ZFont* font = ZGLOBALS::getFont())
+			{
+				if (HFONT hFont = font->getFont())
+				{
+					SendMessage(hwnd, WM_SETFONT, (WPARAM)hFont, 0);
+				}
+			}
+
+			TVINSERTSTRUCT tvis = { 0, TVI_LAST, { TVIF_PARAM|TVIF_TEXT } };
+			tvis.item.pszText = LPSTR_TEXTCALLBACK;
+			AddItems(hwnd, &tvis, (PUCHAR)lpCreateParams);
+			return hwnd;
+		}
+
+		return 0;
+	}
+
+	virtual LRESULT OnNotify(LPNMHDR lpnm)
+	{
+		if (lpnm->idFrom != ID_TV)
+		{
+			return 0;
+		}
+
+		switch (lpnm->code)
+		{
+		case NM_RCLICK:
+			if (HMENU hmenu = LoadMenu((HINSTANCE)&__ImageBase, MAKEINTRESOURCE(IDR_MENU2)))
+			{
+				POINT pt;
+				GetCursorPos(&pt);
+
+				HMENU hSubMenu = GetSubMenu(hmenu, 0);
+
+				switch (TrackPopupMenu(hSubMenu, TPM_RETURNCMD, pt.x, pt.y, 0, lpnm->hwndFrom, 0))
+				{
+				case ID_0_COPY:
+					Copy(lpnm->hwndFrom);
+					break;
+				}
+			}
+
+			return TRUE;
+
+		case TVN_GETDISPINFOW:
+			if (reinterpret_cast<NMTVDISPINFOW*>(lpnm)->item.mask & TVIF_TEXT)
+			{
+				PSTR psz = (PSTR)reinterpret_cast<NMTVDISPINFOW*>(lpnm)->item.lParam;
+				ULONG len = *psz++;
+
+				if (ULONG cchTextMax = reinterpret_cast<NMTVDISPINFOW*>(lpnm)->item.cchTextMax)
+				{
+					PWSTR pszText = reinterpret_cast<NMTVDISPINFOW*>(lpnm)->item.pszText;
+					pszText[MultiByteToWideChar(CP_UTF8, 0, psz, len, pszText, cchTextMax - 1)] = 0;
+				}
+			}
+			break;
+		}
+		return 0;
+	}
+public:
+	ZTviTree(ZDocument* pDoc) : ZView(pDoc)
+	{
+	}
+};
+
+PCWSTR PathGetFileName(PCWSTR lpFileName)
+{
+	PWSTR name = wcsrchr(lpFileName, '\\'), name2 = wcsrchr(lpFileName, '/');
+
+	if (name < name2)
+	{
+		name = name2;
+	}
+
+	return name ? name + 1 : lpFileName;
+}
+
+class ZTviDoc : public ZDocument
+{
+	PSTR _pszTvi = 0;
+	ULONG _cbTvi = 0;
+	ULONG _cch = 0;
+	ULONG _maxlevel = 0;
+
+	~ZTviDoc()
+	{
+		if (_pszTvi)
+		{
+			delete [] _pszTvi;
+		}
+	}
+
+	virtual BOOL IsCmdEnabled(WORD /*cmd*/)
+	{
+		return false;
+	}
+
+	virtual LRESULT OnCmdMsg(WPARAM /*wParam*/, LPARAM /*lParam*/)
+	{
+		return 0;
+	}
+
+public:
+
+	void Copy(HWND hwnd)
+	{
+		if (ULONG cch = _cch)
+		{
+			if (OpenClipboard(hwnd))
+			{
+				EmptyClipboard();
+				if (HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, cch * sizeof(WCHAR)))
+				{
+					ED e { (PWSTR)GlobalLock(hg), cch };
+					ULONG maxlevel = _maxlevel;
+					PSTR prefix = (PSTR)alloca(maxlevel + 1);
+					memset(prefix, '\t', maxlevel);
+					prefix[maxlevel] = 0;
+					PCUCHAR psz = Dump(prefix + maxlevel, (PCUCHAR)_pszTvi, &e);
+
+					GlobalUnlock(hg);
+					if (!psz || !SetClipboardData(CF_UNICODETEXT, hg)) GlobalFree(hg);
+				}
+				CloseClipboard();
+			}
+		}
+	}
+
+	HRESULT Open(HWND hwnd)
+	{
+		HRESULT hr;
+
+		OPENFILENAMEW ofn = { sizeof(ofn) };
+
+		if (ofn.lpstrFile = new WCHAR[0x8000])
+		{
+			*ofn.lpstrFile = 0;
+			ofn.hwndOwner = hwnd;
+			ofn.nMaxFile = 0x8000;
+			ofn.lpstrFilter = L"TVI Files\0*.tvi\0";
+			ofn.lpstrDefExt = L"tvi";
+			ofn.Flags = OFN_DONTADDTORECENT|OFN_ENABLESIZING|
+				OFN_EXPLORER|OFN_FORCESHOWHIDDEN|OFN_NOVALIDATE|
+				OFN_PATHMUSTEXIST|OFN_HIDEREADONLY|OFN_FILEMUSTEXIST;
+
+			if (GetOpenFileNameW(&ofn))
+			{
+				hr = Open(ofn.lpstrFile);
+			}
+			else
+			{
+				hr = HRESULT_FROM_WIN32(ERROR_CANCELLED);
+			}
+
+			delete [] ofn.lpstrFile;
+		}
+		else
+		{
+			hr = E_OUTOFMEMORY;
+		}
+
+		return hr;
+	}
+
+	HRESULT Open(PCWSTR lpFileName)
+	{
+		HANDLE hFile = CreateFileW(lpFileName, FILE_GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			return GetLastHresult();
+		}
+
+		FILE_STANDARD_INFORMATION fsi;
+		IO_STATUS_BLOCK iosb;
+
+		NTSTATUS status = NtQueryInformationFile(hFile, &iosb, &fsi, sizeof(fsi), FileStandardInformation);
+		
+		if (0 <= status)
+		{
+			if (fsi.EndOfFile.QuadPart - 1 < 0x1000000)
+			{
+				if (PSTR buf = new char[fsi.EndOfFile.LowPart])
+				{
+					if (0 <= (status = NtReadFile(hFile, 0, 0, 0, &iosb, buf, fsi.EndOfFile.LowPart, 0, 0)))
+					{
+						AUX a((ULONG)iosb.Information);
+
+						if (IsValidTvi(0, (PUCHAR)buf, (ULONG)iosb.Information, &a) && !a.cb)
+						{
+							if (a.MaxLevel - 1 < 0x40)
+							{
+								_maxlevel = a.MaxLevel;
+								_cch = a.len + 1;
+							}
+
+							if (ZTviTree* p = new ZTviTree(this))
+							{
+								if (p->Create(PathGetFileName(lpFileName), buf))
+								{
+									AddView(p);
+								}
+								p->Release();
+							}
+
+							_pszTvi = buf, _cbTvi = (ULONG)iosb.Information, buf = 0;
+						}
+						else
+						{
+							status = STATUS_INVALID_IMAGE_FORMAT;
+						}
+					}
+					if (buf) delete [] buf;
+				}
+				else
+				{
+					status = STATUS_NO_MEMORY;
+				}
+			}
+			else
+			{
+				status = STATUS_FILE_TOO_LARGE;
+			}
+		}
+
+		NtClose(hFile);
+
+		return HRESULT_FROM_NT(status);
+	}
+};
+
+void ZTviTree::Copy(HWND hWnd)
+{
+	static_cast<ZTviDoc*>(_pDocument)->Copy(hWnd);
+}
+
+class ZMainWnd : public ZMDIFrameWnd
+{
+	virtual LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+		case WM_COMMAND:
+			switch (wParam)
+			{
+			case ID_EXIT:
+				DestroyWindow(hwnd);
+				break;
+			case ID_OPEN:
+				lParam = E_OUTOFMEMORY;
+				if (ZTviDoc* p = new ZTviDoc)
+				{
+					lParam = p->Open(hwnd);
+					p->Release();
+				}
+				if (0 > lParam)
+				{
+					ShowErrorBox(hwnd, L"Fail Open Document", (HRESULT)lParam, MB_OK);
+				}
+				break;
+			}
+			break;
+		}
+		return __super::WindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	virtual PCUNICODE_STRING getPosName()
+	{
+		STATIC_UNICODE_STRING_(sMainWnd);
+		return &sMainWnd;
+	}
+};
+
+void zmain()
+{
+	ZGLOBALS globals;
+	ZApp app;
+	ZRegistry reg;
+	ZFont font(TRUE);
+	
+	if (0 <= reg.Create(L"Software\\{AB188E71-FEB0-49b1-8B94-C646B20A9948}") && font.Init())
+	{
+		HWND hwnd = 0;
+
+		if (ZMainWnd* p = new ZMainWnd)
+		{
+			hwnd = p->ZSDIFrameWnd::Create(L"TVI View", (HINSTANCE)&__ImageBase, MAKEINTRESOURCE(IDR_MENU1));//
+
+			p->Release();
+		}
+
+		if (hwnd)
+		{
+			app.Run();
+		}
+	}
+}
+
+void WINAPI ep(void*)
+{
+	initterm();
+	zmain();
+	destroyterm();
+	ExitProcess(0);
+}
+
+_NT_END
